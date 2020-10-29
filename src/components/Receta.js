80f3ff08a6257c45e486aebc93a329d801caaea2
Alan <alanicio98@outlook.com>
2020-10-28 19:29:58 -0600

    antes de regresar a query params

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -9,6 +9,12 @@ const Receta = ({receta}) => {
         <h2 className="card-header">
           {bebida}
         </h2>
+
+        <img className="card-img-top" src={foto} alt={`Imagen de ${bebida}`} />
+
+        <div className="card-body">
+          <button type="button" className="btn btn-block btn-primary" >Ver receta</button>
+        </div>
       </div>
     </div>
   )
