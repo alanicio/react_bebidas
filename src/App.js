5700f86aaed09e35056a186eb146e5f125e157c8
Alan <alanicio98@outlook.com>
2020-10-29 17:56:08 -0600

    Se obtiene receta de la API

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListaRecetas from "./components/ListaRecetas";
 import CategoriasProvider from "./context/CategoriasContext";
+import ModalProvider from "./context/ModalContext";
 import RecetasProvider from "./context/RecetasContext";
 
 function App() {
   return (
     <CategoriasProvider>
       <RecetasProvider>
-        <Header />
-
-        <div className="container mt-5">
-          <div className="row">
-            <Formulario />
+        <ModalProvider>
+          <Header />
+          <div className="container mt-5">
+            <div className="row">
+              <Formulario />
+            </div>
+            <ListaRecetas />
           </div>
-          <ListaRecetas />
-        </div>
+        </ModalProvider>
       </RecetasProvider>
     </CategoriasProvider>
   );
--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { ModalContext } from "../context/ModalContext";
 
-const Receta = ({receta}) => {
-  const {bebida, foto} = receta;
+const Receta = ({ receta }) => {
+  const { guardarIdReceta } = useContext(ModalContext);
+  const { id, bebida, foto } = receta;
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
-        <h2 className="card-header">
-          {bebida}
-        </h2>
+        <h2 className="card-header">{bebida}</h2>
 
         <img className="card-img-top" src={foto} alt={`Imagen de ${bebida}`} />
 
         <div className="card-body">
-          <button type="button" className="btn btn-block btn-primary" >Ver receta</button>
+          <button
+            type="button"
+            className="btn btn-block btn-primary"
+            onClick={() => guardarIdReceta(id)}
+          >
+            Ver receta
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Receta.propTypes = {
   receta: PropTypes.object.isRequired,
-}
+};
 
-export default Receta
+export default Receta;
